c2badde105150f9f31cd1c0ebee156b5956b033f
Vikas Patel <vikaspatelp83@gmail.com>
2019-10-18 02:54:58 +0530

    Updated and added share button

--- app/src/main/java/com/vikaspatelp83/apps/villageprogrammer/MainActivity.java
+++ app/src/main/java/com/vikaspatelp83/apps/villageprogrammer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.vikaspatelp83.apps.villageprogrammer;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,6 +32,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
@@ -59,6 +64,13 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getIntent();
+        // tranparent status bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.red));
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseAuth.getCurrentUser();
         // browser-start
@@ -177,6 +189,7 @@ public class MainActivity extends AppCompatActivity
         }
         else if(id == R.id.action_share){
             Toast.makeText(MainActivity.this, "Shared", Toast.LENGTH_SHORT).show();
+            share();
         }
         else if(id == R.id.action_exit){
             Toast.makeText(MainActivity.this,"Exiting App",Toast.LENGTH_SHORT).show();
@@ -217,6 +230,29 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    public void share(){
+        Intent myIntent = new Intent(Intent.ACTION_SEND);
+        myIntent.setType("text/plain");
+        String body = "Read the complete post from here \n";
+        String shareUrl = browser.getUrl();
+        body = body + shareUrl;
+        String sub = "Village Programmer Shared Post";
+        myIntent.putExtra(Intent.EXTRA_SUBJECT,sub);
+        myIntent.putExtra(Intent.EXTRA_TEXT,body);
+        startActivity(Intent.createChooser(myIntent, "Share Using"));
+    }
+
     @Override
     protected void onRestoreInstanceState(Bundle savedState) {
         super.onRestoreInstanceState(savedState);
